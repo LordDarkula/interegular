800677666a9490c62bf6768226fd2bdcf45899c5
Aubhro Sengupta <hello@aubhro.com>
2025-07-10 13:29:24 -0700

    Separate loops in accepts to avoid anything else check every iteration

--- interegular/fsm.py
+++ interegular/fsm.py
@@ -228,22 +228,34 @@ class FSM:
             alphabet will be converted to `fsm.anything_else`.
         """
         state = self.initial
-        anything_else_in_alphabet = anything_else in self.alphabet
+        if anything_else in self.alphabet:
         
-        for symbol in input_str:
-            if anything_else_in_alphabet and not symbol in self.alphabet:
-                symbol = anything_else
+            for symbol in input_str:
+                if not symbol in self.alphabet:
+                    symbol = anything_else
+                    
+                if state not in self.map:
+                    return False
                 
-            if state not in self.map:
-                return False
-            
-            transition = self.alphabet[symbol]
+                transition = self.alphabet[symbol]
 
-            # Missing transition = transition to dead state
-            if transition not in self.map[state]:
-                return False
+                # Missing transition = transition to dead state
+                if transition not in self.map[state]:
+                    return False
 
-            state = self.map[state][transition]
+                state = self.map[state][transition]
+        else:
+            for symbol in input_str:
+                if state not in self.map:
+                    return False
+                
+                transition = self.alphabet[symbol]
+
+                # Missing transition = transition to dead state
+                if transition not in self.map[state]:
+                    return False
+
+                state = self.map[state][transition]
         return state in self.finals
 
     def __contains__(self, string):
